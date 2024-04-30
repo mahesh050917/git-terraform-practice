725e1da3800f3348b4eeb004e99ad5adca8c40e0
mahg0509 <mahg0509@outlook.com>
2024-04-30 19:58:47 +0530

    merge commits

--- inputvar.tf
+++ inputvar.tf
@@ -7,12 +7,9 @@ variable "address_space" {
   default = ["10.0.0.0/16"]
 }
 
-<<<<<<< HEAD
 
 
-=======
 variable "address_space1" {
   type    = list(string)
   default = ["11.0.0.0/16"]
 }
->>>>>>> dbe46b08c390d8cb684fe0cd1232744d2eb1187e
